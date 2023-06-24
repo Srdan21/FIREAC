2d8b3ae68aaa92b33d56db9b61ebbe5acade8a24
Srdan21 <81490336+Srdan21@users.noreply.github.com>
2023-06-23 22:31:04 -0300

    Update fire-white.lua

--- whitelists/fire-white.lua
+++ whitelists/fire-white.lua
@@ -7,14 +7,6 @@ WhiteList = {
 
 }
 
--------------------- This is for adding admins for the admin menu ------------------
-Admins = {
-    '7835767148521717**',                                -- Discord ID
-    'steam:1100001476d05**',                             -- Steam Hex
-    '192.168.1.**',                                      -- IP Address
-    'license2:500a67097ce3c274569c773bc41974d3c28380**', -- FiveM License
-}
-
 -------------------- This is for command unban players (/funban) ------------------
 UnBan = {
     '7835767148521717**',                                -- Discord ID
